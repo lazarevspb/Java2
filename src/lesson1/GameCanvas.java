1f6234e9fe274f43fc3956829eebb30057ebdf30
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2020-08-26 22:56:39 +0300

    lesson1 wip

---
+++ src/lesson1/GameCanvas.java
@@ -0,0 +1,54 @@
+package lesson1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameCanvas extends JPanel {
+    private long lastFrameTime;
+    private MainCircles controller;
+
+    GameCanvas(MainCircles controller) {
+        this.controller = controller;
+//        setBackground(Color.MAGENTA); // TODO: 26.08.2020 удалить
+        lastFrameTime = System.nanoTime();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        long currentTime = System.nanoTime();
+        float deltaTime = (currentTime - lastFrameTime) * 0.000_000_001f;
+        controller.onDrawFrame(this, g, deltaTime);
+
+        lastFrameTime = currentTime;
+        try {
+            Thread.sleep(17);// обновление 60 раз в сек.
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        repaint();
+    }
+
+    /**
+     * Левая граница канвы;
+     *
+     * @return
+     */
+    public int getLeft() {
+        return 0;
+    }
+
+    public int getRight() {
+        return getWidth() - 1;
+    }
+
+    public int getTop() {
+        return 0;
+    }
+
+    public int getBottom() {
+        return getHeight() - 1;
+    }
+
+
+}
