8a9a8ca54784ccb4ce99edae7882fb375cfe097f
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2020-09-10 19:16:27 +0300

    lesson5 final commit: Minor bug fixes and code refactoring.

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -8,35 +8,37 @@ import java.util.Arrays;
  */
 
 public class Main {
-    static final int size = 10000000;
-    static final int h = size / 2;
+    static final int SIZE = 10_000_000;
+    static final int HALF_LENGTH = SIZE / 2;
     public static void main(String[] args) throws InterruptedException {
 
         long a = System.currentTimeMillis();
         methodOne();
         methodTwo();
-        printingTheSpentTime(a, "method main");
+        printingTheSpentTime(a, "total execution time");
     }
 
     static void methodOne() {
-        float[] arr = createsAOneDimensionalLongArray();
-        long a = System.currentTimeMillis();
+        float[] arr = createsAOneDimensionalLongArray(SIZE);
+
 
         fillsThisArrayWithOnes(arr);
+
+        long a = System.currentTimeMillis();
         methodThatCountsSomething(arr);
 
         printingTheSpentTime(a, "methodOne");
     }
 
     private static void methodTwo() throws InterruptedException {
-        float[] arr = createsAOneDimensionalLongArray();
-        float[] a1 = new float[h];
-        float[] a2 = new float[h];
+        float[] arr = createsAOneDimensionalLongArray(SIZE);
+        fillsThisArrayWithOnes(arr);
+        float[] a1 = createsAOneDimensionalLongArray(HALF_LENGTH);
+        float[] a2 = createsAOneDimensionalLongArray(HALF_LENGTH);
 
         long a = System.currentTimeMillis();
         Thread thread0 = new Thread(() -> assemblyDisassemblyArray(arr, a1, 0));
-
-        Thread thread1 = new Thread(() -> assemblyDisassemblyArray(arr, a2, h));
+        Thread thread1 = new Thread(() -> assemblyDisassemblyArray(arr, a2, HALF_LENGTH));
 
         thread0.start();
         thread1.start();
@@ -47,17 +49,14 @@ public class Main {
         printingTheSpentTime(a, "methodTwo");
     }
 
-    private static void printingTheSpentTime(long a, String methodName) {
-        System.out.printf("%s, %s: %d ms to run. \n", Thread.currentThread().getName(), methodName, (System.currentTimeMillis() - a));
+    private static void printingTheSpentTime(long a, String text) {
+        System.out.printf("%s: %d ms to run. \n", text, (System.currentTimeMillis() - a));
     }
 
     private static void assemblyDisassemblyArray(float[] sourceArray, float[] destinationArray, int startingElementIndex) {
-
-        long a = System.currentTimeMillis();
-        System.arraycopy(sourceArray, startingElementIndex, destinationArray, 0, h);
-        fillsThisArrayWithOnes(destinationArray);
+        System.arraycopy(sourceArray, startingElementIndex, destinationArray, 0, HALF_LENGTH);
         methodThatCountsSomething(destinationArray);
-        System.arraycopy(destinationArray, 0, sourceArray, startingElementIndex, h);
+        System.arraycopy(destinationArray, 0, sourceArray, startingElementIndex, HALF_LENGTH);
     }
 
     static float[] methodThatCountsSomething(float[] arr) {
@@ -68,10 +67,10 @@ public class Main {
     }
 
     static void fillsThisArrayWithOnes(float[] arr) {
-        Arrays.fill(arr, 1);
+        Arrays.fill(arr, (float) 1.0);
     }
 
-    static float[] createsAOneDimensionalLongArray() {
+    static float[] createsAOneDimensionalLongArray(int size) {
         return new float[size];
     }
 }
